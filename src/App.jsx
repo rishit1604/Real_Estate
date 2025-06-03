23cd10d095e319902c79724f879cdfc75f4b7fb1
Rishit <202203021@daiict.ac.in>
2025-06-04 01:35:54 +0530

    create pages and rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import SignOut from './pages/SignOut';
 
 export default function App(){
-  return  <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>;
+  return (
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sign-in" element={<SignIn/>}/>
+            <Route path="/sign-out" element={<SignOut/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/about" element={<About/>}/>
+          </Routes>
+    </BrowserRouter>
+  );
 }
