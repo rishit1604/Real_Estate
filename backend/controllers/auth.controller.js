07f6d3c1134ba10736c1c14692273f6a661b5787
Rishit <202203021@daiict.ac.in>
2025-06-04 17:09:02 +0530

    started with signup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req,res) =>{
+    const {username,email,password} = req.body;
+    const hashPassword = bcryptjs.hashSync(password,10);
+    const newUser = new User({username,email,password:hashPassword});;
+    await newUser.save();
+    res.status(201).json('User created Succesfully');
+};
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
+mongoose.connect("mongodb+srv://Rishit164:rhu78743@real-estate.abdbenm.mongodb.net/?retryWrites=true&w=majority&appName=Real-Estate").then(()=>{
+    console.log("connected");
+})
+.catch(()=>{
+    console.log(err);
+});
 const app = express();
 
 app.listen(3000,() => {
     console.log("Started Server");
 });
+
+app.use(express.json());
+app.use('/backend/user',userRouter);
+app.use('/backend/auth',authRouter);
