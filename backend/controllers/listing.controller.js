408e53639d33ed28c42ea56ca3549b2f552bb98d
Rishit <202203021@daiict.ac.in>
2025-06-15 00:35:06 +0530

    COmpleted listing edit

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 
 export const createListing= async (req, res,next) => {
@@ -20,14 +21,55 @@ export const deleteListing = async (req, res, next) => {
         const listing = await Listing.findById(id);
 
         if (!listing) {
-            return next({ status: 404, message: 'Listing not found' });
+            return next(errorHandler(404,'Listing not found' ));
         }
 
         if(req.user.id !== listing.userRef ) {
-            return  next({ status: 403, message: 'You are not authorized to delete this listing' });
+            return  next(errorHandler(403,'You are not authorized to delete this listing' ));
         }
+
         await Listing.findByIdAndDelete(id);
-        return res.status(200).json({ message: 'Listing deleted successfully' });
+        return res.status(200).json('Listing deleted successfully' );
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+     
+     
+     try {
+          const { id } = req.params;
+          const listing = await Listing.findById(id);
+
+          if( !listing ) {
+               return next(errorHandler(404,'No Such Listing Exist!' ));
+          }
+
+          if(req.user.id !== listing.userRef ) {
+               return next(errorHandler(403,'You are not authorized to update this listing' ));
+          }
+
+          const updatedListing = await Listing.findByIdAndUpdate(id,req.body, {
+               new: true
+               
+          });
+          res.status(200).json(updatedListing);
+     } catch (error) {
+          next(error);
+     }
+}
+
+export const showListing = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findById(id);
+
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found'));
+        }
+
+        return res.status(200).json(listing);
     } catch (error) {
         next(error);
     }
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, showListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/user.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
-
+router.post('/update/:id', verifyToken, updateListing);  
+router.get('/get/:id',showListing);
 export default router;
